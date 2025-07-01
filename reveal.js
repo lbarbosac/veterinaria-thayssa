88487e30dabf0d6ae3ade6941e27cf68bf70274e
Lucas Barbosa <lucas2304barbosa@gmail.com>
2025-07-01 09:15:40 -0300

    2

---
+++ reveal.js
@@ -0,0 +1,53 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// topo 
+
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+        duration: 2000,
+        distance: '90px',
+        delay: 500
+})
+
+// servicos
+
+revelar.reveal('.servico',
+{
+            duration: 2000,
+            distance: '90px',
+            delay: 1000
+})
+
+// sobre
+
+revelar.reveal('.sobre',
+{
+            duration: 2000,
+            distance: '90px',
+            delay: 500 
+})
+
+// footer confianca
+
+revelar.reveal('.confianca',
+    {
+            duration: 2000,
+            distance: '90px',
+            delay: 250
+    })
+
+// footer beneficios
+
+revelar.reveal('.beneficios div',
+    {
+            duration: 2000,
+            distance: '90px',
+            delay: 250
+    })
